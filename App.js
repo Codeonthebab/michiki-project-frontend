d7f766b62d33668f83813e4b3fc31ee0333ac6a4
normalization00 <dlwpgks1@gmail.com>
2025-07-18 01:44:47 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Main from "./Main";
+
+function App() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        minHeight: "100vh",
+        background: "#fff9f0"
+      }}
+    >
+      <Main />
+    </div>
+  );
+}
+
+export default App;
